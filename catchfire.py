cebf56385370a8c8599199fe5a5b9b8666a99e8e
yuki-matsumura <matsumura@MP01.local>
2019-05-23 23:39:44 +0900

    0523

--- catchfire.py
+++ catchfire.py
@@ -5,8 +5,11 @@ corepath = "/sys/devices/platform/coretemp.0/hwmon/hwmon2/"
 core = glob.glob(corepath + "temp*_input")
 num = len(core)
 
+now = []
 for i in range(num):
   i = i + 1
-  for input in open(corepath + "temp" + str(i) + "_input", "r"):
+  for coretemp in open(corepath + "temp" + str(i) + "_input", "r"):
+    now.append(int(coretemp))
     for crit in open(corepath + "temp" + str(i) + "_crit", "r"):
-      print("Core" + str(i) + ":  " + input[:2] + "." + input[2:5] + "  Crit: +" + crit[:2] + "." + crit[2:5])
+      print("Core" + str(i) + ":  " + coretemp[:2] + "." + coretemp[2:5] + "  Crit: +" + crit[:2] + "." + crit[2:5])
+print now
