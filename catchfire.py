ed45ceb6f499c0d5083dd991114c6698ab06a144
Yuki-Matsumura <matsumura42@gmail.com>
2019-05-26 22:12:24 +0900

    0526

--- catchfire.py
+++ catchfire.py
@@ -7,7 +7,8 @@ corepath = "/sys/devices/platform/coretemp.0/hwmon/hwmon2/"
 core = glob.glob(corepath + "temp*_input")
 global num
 num = len(core)
-
+global high
+high = []
 
 ##### Get CPU info #####
 def cpuinfo():
@@ -32,28 +33,56 @@ def nowtemp():
       now.append(int(coretemp))
 ##### Get now Temp #####
 
-
-##### Get high Temp #####
-def hightemp():
-  global high
-  high = []
+##### Get Maximum Temp #####
+def maximumtemp():
+  global maximum
+  maximum = []
   i = 0
   for i in range(num):
     i = i + 1
     for crit in open(corepath + "temp" + str(i) + "_crit", "r"):
-      high.append(int(crit))
+      maximum.append(int(crit))
+##### Get Maximum Temp #####
+
+##### Get high Temp #####
+def hightemp():
+  if not high:
+    for i in range(num):
+      high.append(0)
+    difftemp = high
+  nowtemp()
+  global difftemp
+  difftemp = [max(nowdifftemp) for nowdifftemp in zip(now, difftemp)]
 ##### Get high Temp #####
 
 
+##### Main #####
 def action():
-  hightemp()
-  while True:
+  try:
+    maximumtemp()
+    while True:
+      pass
+      os.system('clear')
+      hightemp()
+      print("now:  ")
+      print now
+      print("high: ")
+      print difftemp
+      print("max:  ")
+      print maximum
+      time.sleep(2)
+  except KeyboardInterrupt:
     os.system('clear')
-    nowtemp()
-    print now
-    print high
-    time.sleep(2)
+    print("End script.")
+##### Main #####
+
 
 action()
 
+
+class color:
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    END = '\033[0m'
 #print("Core" + str(i) + ":  " + coretemp[:2] + "." + coretemp[2:5] + "  Crit: +" + crit[:2] + "." + crit[2:5])
